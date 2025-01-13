ecacfe4c1af1ef8475838d372874e34ee597038d
JohnHarrow <52578545+JohnHarrow@users.noreply.github.com>
2025-01-13 20:10:53 +0000

    Edited video service

--- video-service/index.js
+++ video-service/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;  
 
 const videos = [
-  { id: 1, title: 'The Jester', genre: 'Boring', s3Url: 'https://bucket-for-assignment-2.s3.us-east-1.amazonaws.com/32962_1411112556_s01_The_Jester_Narren_Grappen.mp4' },
+  { id: 1, title: 'The Jester', genre: 'Very Boring', s3Url: 'https://bucket-for-assignment-2.s3.us-east-1.amazonaws.com/32962_1411112556_s01_The_Jester_Narren_Grappen.mp4' },
   { id: 2, title: 'Bobs Electric Theatre', genre: 'Slightly More Boring', s3Url: 'https://bucket-for-assignment-2.s3.us-east-1.amazonaws.com/Bobs+Electric+Theater+-1906.mp4' }
 ];  
  
